a2bbfd9fd80b0a9701369ee22d1038fe3f488824
SylvanusNK <nkamabasil@gmail.com>
2022-07-01 16:44:17 -0700

    Program that print lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns alpahbet a-z
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char d = 'a';
+
+	while (d <= 'z'0)
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
