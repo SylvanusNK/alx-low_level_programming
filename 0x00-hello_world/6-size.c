e9a693c8d13880efeed23e5be530bdd9c30d2a32
SylvanusNK <nkamabasil@gmail.com>
2022-06-30 17:47:13 -0700

    Script that prints  size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,8 +13,7 @@ int main(void)
 
 	printf("Size of char: %lu byte(s)\n", sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	printf("Size of a long integer: %lu byte(s)\n", sizeof(l));
-	printf("Size of a long long integer: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f)); 
 	return (0);
-}
