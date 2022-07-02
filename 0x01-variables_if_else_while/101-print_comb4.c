411e5b90d7fb27a2daf281d2efbb3de803373d84
SylvanusNK <nkamabasil@gmail.com>
2022-07-02 15:27:30 -0700

    Print three possible combination of three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,2 +1,34 @@
-#include <j:wq
-st
+#include <stdio.h>
+
+/**
+ * main -to print possible outcomes of three numbers
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int n, m, l;
+
+	for (n = 48; n < 58; n++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
