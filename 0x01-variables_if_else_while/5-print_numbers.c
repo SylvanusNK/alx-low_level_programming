383d1263c30cec786f6a03ac409aff5533f449b9
SylvanusNK <nkamabasil@gmail.com>
2022-07-01 17:33:56 -0700

    Program that print digits 0-9 with a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - retur from 0 - 9
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", num)
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
