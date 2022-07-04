841c010606deb930c3501e3cb23cad9ba309a6b1
SylvanusNK <nkamabasil@gmail.com>
2022-07-03 23:59:06 -0700

    Prints single digits on a line

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - prints combination of single digits
- * Return: Always 0 (success)
+* main - Entry point
+* Return: Always 0 (Success/correct)
 */
-
 int main(void)
 {
-
-	int num = '0';
-	int comma = ',';
-
-	while (num <= '9')
-	{
-		putchar(num);
-		if (num < '9')
-		{
-			putchar(comma);
-		}
-		putchar(' ');
-		num++;
-	}
-	return (0);
+int number;
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+if (number == 57)
+{
+break;
+}
+putchar(',');
+putchar (' ');
+}
+putchar('\n');
+return (0);
 }
