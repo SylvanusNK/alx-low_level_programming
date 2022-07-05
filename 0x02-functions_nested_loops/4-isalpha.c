6d26d42a239c2442a3ca2ac8046593810805ec93
SylvanusNK <nkamabasil@gmail.com>
2022-07-05 16:58:51 -0700

    Checks for alpha characters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - check for an alphabetical letter
+* @type: used for consideration
+* Return: return 0 if successful otherwise 1
+*/
+
+int _isalpha(int type);
+{
+	if (type >= 'a' || type >= 'A' && type <= 'z' || type <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
