9d1661a8a0b92d3d7dc3df0ff3dfb3d876a99e11
SylvanusNK <nkamabasil@gmail.com>
2022-07-12 14:25:47 -0700

    Generating passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define GRAPH_MIN 33
+#define GRAPH_MAX 126
+
+/**
+ * main - generate valid passwords for 101-crackme
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int sum = 2772;
+char c;
+srand(time(NULL));
+
+while (sum > GRAPH_MAX)
+{
+c = rand() % (GRAPH_MAX - GRAPH_MIN) + GRAPH_MIN;
+sum -= c;
+
+if (sum < GRAPH_MIN)
+{
+c -= (GRAPH_MIN - sum);
+sum = GRAPH_MIN;
+}
+putchar(c);
+}
+putchar(sum);
+return (0);
+
+}
+Footer
