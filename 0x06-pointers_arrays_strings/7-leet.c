41b23d0de0ec696fb4abc096c0c34f0f9b583c02
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 07:42:06 -0700

    Encoding the string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,12 +5,9 @@
 * @x: string parameter
 * Return: string
 */
-
 char *leet(char *x)
 {
-	int a = 0;
-	int b;
-	int l = 5;
+	int a = o, b, l = 5;
 	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
 	char trw[5] = {'4', '3', '0', '7', '1'};
 
@@ -20,7 +17,7 @@ char *leet(char *x)
 
 		while (b < l)
 		{
-			if (x[a] == tr[b] || x[a] - 32 tr[b])
+			if (x[a] == tr[b] || x[a] - 32 == tr[b])
 				x[a] = trw[b];
 			b++;
 		}
