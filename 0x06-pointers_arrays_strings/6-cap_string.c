a5b67ef58389c7161b55f20c92429a8f66d73ed5
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 07:06:42 -0700

    Capitalizing every letter

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,23 +2,23 @@
 
 /**
 * cap_string - function that capitalizes every character in a string
-* @b: string parameter
+* @x: string parameter
 * Return: string
 */
 
-char *cap_string(char *b)
+char *cap_string(char *x)
 {
 	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 	int len = 13;
 	int a = 0, i;
 
-	while (b[a])
+	while (x[a])
 	{
 		i = 0;
 		while (i < len)
 		{
-			if ((a == 0 || b[a - 1] == spc[i] && (b[a] >= 97 && b[a] <= 122))
-				b[a] = b[a] - 32;
+			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
+				x[a] = x[a] - 32;
 			i++;
 		}
 		a++;
