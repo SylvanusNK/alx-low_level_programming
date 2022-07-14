b8e5c2ad4b9b53fb5c2d388126005666b14b37cb
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 04:13:48 -0700

    Copying files with strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _strncpy - a funtion that copies two strings
+* @dest: first parameter
+* @src: second parameter
+* @n: number of byte
+* Return: a string
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
