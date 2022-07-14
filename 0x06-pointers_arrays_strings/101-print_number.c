011afc2799cbb5c1c8ea3ab05582c3a20ce6481e
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 12:11:02 -0700

    Printing of intergers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: the integer to be printed
+ * Return: void
+*/
+
+void print_number(int n)
+{
+
+if (n > -1)
+{
+if (n > 9)
+{
+print_number(n / 10);
+_putchar('0' + n % 10);
+}
+else
+{
+_putchar('-');
+if (n < -9)
+print_number(n / -10);
+_putchar('0' - n % 10);
+}
+}
