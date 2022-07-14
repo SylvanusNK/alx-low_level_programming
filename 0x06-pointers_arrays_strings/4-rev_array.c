8a814734c633f8441c378e22f007a82762ac10f7
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 08:23:11 -0700

    hot fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,18 +9,16 @@
 
 void reverse_array(int *a, int n)
 {
-	int *ptr, i, new, x;
+	int temp, i = 0;
 
-	ptr = a;
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
 	for (i = 0; i < n; i++)
 	{
-		ptr++;
-	}
-	for (x = 0; x < n / 2; x++)
-	{
-		new = a[x];
-		a[x] = *ptr;
-		*ptr = new;
-		ptr--;
+		_putchar("%d", a[i]);
 	}
 }
