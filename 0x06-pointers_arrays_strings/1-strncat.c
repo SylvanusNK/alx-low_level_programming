d538cd6583486231f850e6ac914f8aed4ed18c3f
SylvanusNK <nkamabasil@gmail.com>
2022-07-14 02:34:31 -0700

    Using strncat to cocatenate

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncat - function that cocatenates strings with n bytes
+* @dest: first parameter
+* @src: second parameter
+* Return: dest string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char dest, src;
+	int n;
+
+	strncat(dest, src, n);
+	return (dest);
+}
