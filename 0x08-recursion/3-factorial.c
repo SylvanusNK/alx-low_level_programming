5ae5393ca8d5cc0348f43140e2cb335d67bb9f60
SylvanusNK <nkamabasil@gmail.com>
2022-07-20 16:15:14 -0700

    Factorial mof a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* factorial - a funtions that prints the factorial of a number
+* @n: given number
+* Return: factorial of a number
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
