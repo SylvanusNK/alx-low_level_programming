d5133c7e391b86ac3ae6ab8b560be6386433218d
SylvanusNK <nkamabasil@gmail.com>
2022-07-20 16:53:39 -0700

    Funtion that prints sqrt of a number recursively

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - a funtion that returns the natural square root of a number
+* @n: given number
+* Return: natural square root of n
+*/
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
