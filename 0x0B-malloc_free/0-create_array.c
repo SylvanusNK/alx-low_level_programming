be34082e34179202270d95c46f933051994fcff7
SylvanusNK <nkamabasil@gmail.com>
2022-07-26 12:30:10 -0700

    creating an array of char using malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* create_array - funtion that creates an array of char
+* @size: size of allocated memory
+* @c: character parameter
+* Return: pointer if succrssful else 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This repo is about dynamic allocation of memory in C programming using malloc and free function.
