62e631e52921d8d1e440a9adbff76eaf53c8c2bf
SylvanusNK <nkamabasil@gmail.com>
2022-08-01 16:04:48 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define _DOG_H_
 
 /**
-* main - creates a struc dog for name age and owner
+* struct dog - creates a struc dog for name age and owner
 * @name: pointer to name of the dog
 * @age: age of dog
 * @onwer: pointer to the gog owner
@@ -10,17 +10,13 @@
 */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
-
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
