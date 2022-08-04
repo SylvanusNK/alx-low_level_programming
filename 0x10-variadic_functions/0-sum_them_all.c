2b44f87cc5bf3a29e5c3283fce0752f864e6f66d
SylvanusNK <nkamabasil@gmail.com>
2022-08-04 04:14:16 -0700

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "function_variadic.h"
+#include "variadic_functions.h"
 
 /**
 * sum_them_all - a function that prints sum of its params
@@ -17,7 +17,6 @@ int sum_them_all(const unsigned int n, ...);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
-
 	va_end(ap);
 	return (sum);
 }
