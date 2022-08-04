5c5b8a3975434f763c191e1abf472550fe377fff
SylvanusNK <nkamabasil@gmail.com>
2022-08-04 04:07:32 -0700

    hot fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,20 +4,19 @@
 * sum_them_all - a function that prints sum of its params
 * @n: numbers to be summed
 * Return: sum of numbers
+*
 */
 
 int sum_them_all(const unsigned int n, ...);
 {
-	va_list ap;
 	unsigned int i;
 	int sum;
+	va_list ap;
 
 	va_start(ap, n);
 
 	sum = 0;
 
-	if (n == 0)
-		return (0);
 	for (i = 0; i < n; i++)
 		sum += va_arg(ap, int);
 
