eb0e89ab11fbf738fff0716b4037c03a7a650b83
SylvanusNK <nkamabasil@gmail.com>
2022-08-04 03:26:16 -0700

    summing up numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "function_variadic.h"
+#include <stdarg.h>
+/**
+* sum_them_all - a function that prints sum of its params
+* @n: numbers to be summed
+* Return: sum of numbers
+*/
+
+int sum_them_all(const unsigned int n, ...);
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, const unsigned int);
+
+	va_end(ap)
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This repo explians vividly how to create functions that takes many args in a function and how to use them.
