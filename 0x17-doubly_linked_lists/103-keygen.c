a78916759bbbee57f62ef6d4fe10ca9488f34769
SylvanusNK <nkamabasil@gmail.com>
2022-09-29 16:43:52 -0700

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 /**
  * main - generate a key depending on a username for crackme5
  * @argc: number of arguments passed
@@ -9,12 +8,12 @@
  *
  * Return: 0 on success, 1 on error
  */
+
 int main(int argc, char *argv[])
 {
 	unsigned int i, b;
 	size_t len, add;
-	char *l = "A-CHRDw87NS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-
+	char *l = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char p[7] = "      ";
 
 	if (argc != 2)
